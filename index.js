f5946b4b3edd6c2c6abf53f05f8c190fab795aac
alamin20cse <md.alamin165086@gmail.com>
2024-12-04 03:24:23 +0600

    posted to db

--- index.js
+++ index.js
@@ -29,12 +29,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const campignCollection=client.db('campignDB').collection('campign');
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+
+    app.post('/campign',async(req,res)=>{
+        const newCampign=req.body;
+        console.log(newCampign);
+        const result=await campignCollection.insertOne(newCampign);
+        res.send(result);
+
+        
+    })
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   
   }
 }
 run().catch(console.dir);
