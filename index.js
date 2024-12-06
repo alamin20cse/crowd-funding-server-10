01aa2c6e95062a727f054dc55233f3607f638c9d
alamin20cse <md.alamin165086@gmail.com>
2024-12-06 21:03:33 +0600

    delet operation done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port =process.env.PORT || 4000;
 
@@ -50,6 +50,14 @@ async function run() {
 
     })
 
+
+    app.delete('/campign:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)} ;
+      const result=await campignCollection.deleteOne(query);
+      res.send(result);
+
+    })
 
 
 
